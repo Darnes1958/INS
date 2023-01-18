e4dbda8403fedb5b64976771a9081dd79b59f56e
Darnes1958 <57214138+Darnes1958@users.noreply.github.com>
2023-01-18 21:09:53 +0200

    update 18/01/2022 pm3

--- resources/views/livewire/manager/user-select.blade.php
+++ resources/views/livewire/manager/user-select.blade.php
@@ -2,7 +2,7 @@
   <select   wire:model="UserNo" id="User_L" class="User_L" >
     <option value="">اختيار من القائمة</option>
     @foreach($UserList as $s)
-      <option value="{{ $s->id }}">{{ $s->name }}</option>
+      <option value="{{ $s->id }}">{{ $s->id }} | {{ $s->name }}</option>
     @endforeach
   </select>
 </div>
