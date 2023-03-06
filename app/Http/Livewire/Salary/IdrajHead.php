2d9459bb8b84f76acc1cbcb73898d875d54a828e
Darnes1958 <57214138+Darnes1958@users.noreply.github.com>
2023-03-07 00:00:56 +0200

    04/03/2023 pm1

---
+++ app/Http/Livewire/Salary/IdrajHead.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Salary;
+
+use Livewire\Component;
+
+class IdrajHead extends Component
+{
+    public function render()
+    {
+        return view('livewire.salary.idraj-head');
+    }
+}
---
+++ app/Http/Livewire/Salary/IdrajTable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Salary;
+
+use Livewire\Component;
+
+class IdrajTable extends Component
+{
+    public function render()
+    {
+        return view('livewire.salary.idraj-table');
+    }
+}
