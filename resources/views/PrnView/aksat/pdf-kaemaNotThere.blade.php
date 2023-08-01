ebca0b658bc78ad08e0f498de217915d70651cb6
Darnes1958 <57214138+Darnes1958@users.noreply.github.com>
2023-08-01 12:09:22 +0200

    kaema

--- resources/views/PrnView/aksat/pdf-kaemaNotThere.blade.php
+++ resources/views/PrnView/aksat/pdf-kaemaNotThere.blade.php
@@ -20,18 +20,23 @@
     <thead style=" font-family: DejaVu Sans, sans-serif; margin-top: 8px;" >
     <tr  style="background: #9dc1d3;" >
       <th style="width: 10%"> القسط</th>
+      <th style="width: 12%"> المطلوب</th>
+      <th style="width: 12%"> المسدد</th>
+      <th style="width: 14%">اجمالي العقود</th>
 
       <th >الإسم</th>
-      <th style="width: 20%">رقم الحساب</th>
-      <th style="width: 20%">رقم العقد</th>
+      <th style="width: 16%">رقم الحساب</th>
+      <th style="width: 10%">رقم العقد</th>
     </tr>
     </thead>
     <tbody style="margin-bottom: 40px; ">
-    @php $sumkst=0; @endphp
+    @php $sumkst=0;$sumsul=0;$sumsul_pay=0;$sumraseed=0; @endphp
     @foreach($res as $key => $item)
       <tr >
         <td> {{number_format($item->kst, 2, '.', '')}} </td>
-
+        <td> {{ $item->raseed }} </td>
+        <td> {{ $item->sul_pay }} </td>
+        <td> {{ $item->sul }} </td>
         <td> {{ $item->name }} </td>
         <td> {{ $item->acc }} </td>
         <td> {{ $item->no }} </td>
@@ -41,12 +46,15 @@
         <label class="page"></label>
         <label> صفحة رقم </label>
       </div>
-      @php $sumkst+=$item->kst; @endphp
+      @php $sumkst+=$item->kst;$sumsul+=$item->sul;$sumsul_pay+=$item->sul_pay;$sumraseed+=$item->raseed; @endphp
     @endforeach
     <tr class="font-size-12 " style="font-weight: bold">
 
 
 
+      <td> {{number_format($sumraseed, 2, '.', ',')}}  </td>
+      <td> {{number_format($sumsul_pay, 2, '.', ',')}}  </td>
+      <td> {{number_format($sumsul, 2, '.', ',')}}  </td>
       <td> {{number_format($sumkst, 2, '.', ',')}}  </td>
       <td>   </td>
       <td>   </td>
