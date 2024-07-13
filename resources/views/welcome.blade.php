af82ad8fffc8e25a963fb0a9b053c285d78b2238
Darnes1958 <abdel_ati_m@yahoo.com>
2024-07-13 18:31:28 +0200

    any

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,10 +25,10 @@
         <div >
             @if (Route::has('login'))
 
-                <div style="width: 200px;height: 120px; padding-top: 20px; margin: auto;">
+                <div style="width: 40px;height: 120px; padding-top: 20px; margin: auto;">
                     @auth
                         <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">الصفحة الرئيسية</a>
-                        <a href="{{ url('/logout') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">خروج</a>
+
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline text-lg">دخول</a>
                     @endauth
@@ -54,6 +54,7 @@
                 <div class="hidden fixed top-0 right-0  px-6 py-4 sm:block">
                     @auth
                         <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">الصفحة الرئيسية</a>
+                        <a href="{{ url('/logout') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">خروج</a>
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline text-lg">دخــول</a>
                     @endauth
