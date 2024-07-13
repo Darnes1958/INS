765911d21284410e4497dd6d24cb4afc2948cc23
Darnes1958 <abdel_ati_m@yahoo.com>
2024-07-13 19:01:37 +0200

    any

--- resources/views/livewire/admin/admin-page.blade.php
+++ resources/views/livewire/admin/admin-page.blade.php
@@ -68,9 +68,7 @@
      @livewire('admin.inp-company')
      @livewire('admin.rep-company')
      @livewire('admin.rep-users')
-     @livewire('admin.manage-roles')
 
-     @livewire('admin.rep-roles')
      @livewire('admin.kaema')
      @livewire('admin.mahjoza')
      @livewire('admin.from-excel')
