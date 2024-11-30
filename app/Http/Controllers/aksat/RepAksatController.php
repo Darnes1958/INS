6d70ded8dd772c1a78a8144b36b6b86aeb9c9cfd
Darnes1958 <abdel_ati_m@yahoo.com>
2024-11-30 22:14:21 +0200

    any

--- app/Http/Controllers/aksat/RepAksatController.php
+++ app/Http/Controllers/aksat/RepAksatController.php
@@ -278,15 +278,14 @@ class RepAksatController extends Controller
       ->orderby($request->column,$request->sort)
       ->get();
    if ($request->by=='Bank')  {
-       rep_bank::where('bank', '=', $request->bank_no)
+       $res=rep_bank::where('bank', '=', $request->bank_no)
            ->orderby($request->column,$request->sort)
            ->get();
        $bank_name=bank::find($request->bank_no)->bank_name;
    }
    else {
-       rep_bank::whereIn('bank', function($q) use ($request) {
+       $res=rep_bank::whereIn('bank', function($q) use ($request) {
            $q->select('bank_no')->from('bank')->where('bank_tajmeeh',$request->TajNo);})
-           ->orderby($request->column,$request->sort)
            ->get();
 
        $bank_name=BankTajmeehy::find($request->TajNo)->TajName;
