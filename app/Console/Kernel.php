6799928052c457e4f11a99b20607045ba0168ce3
unknown <bdel_ati_m@yahoo.com>
2025-04-17 10:33:55 +0200

    any

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('message:daily')->dailyAt('01:00');
+      // $schedule->command('message:daily')->dailyAt('01:00');
     }
 
     /**
