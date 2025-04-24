93fc9f05e1e40e6c428dcd14e9d02607b98836f3
unknown <bdel_ati_m@yahoo.com>
2025-04-24 11:07:31 +0200

    any

--- resources/views/admin/body/slidebar.blade.php
+++ resources/views/admin/body/slidebar.blade.php
@@ -93,7 +93,7 @@
                         <li><a href="{{route('itemdamage')}}" class="link-dark d-inline-flex text-decoration-none rounded font-size-14 h4 my-0 py-0">توحيد صنف</a></li>
                         @endcan
                       @if (Auth()->User()->company=='Motahedon' || Auth()->User()->company=='Taleb' || Auth()->User()->company=='Bentaher2'
-                         || Auth()->User()->company=='Sohol')
+                         || Auth()->User()->company=='Sohol' || Auth()->User()->company=='Boshlak')
                         <li><a href="{{route('jaradraseed')}}" class="link-dark d-inline-flex text-decoration-none rounded font-size-14 h4 my-0 py-0">تعديل ارصدة بعد الجرد</a></li>
                        @endif
 
