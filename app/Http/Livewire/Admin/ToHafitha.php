3464fd6df2e3fecb0fc617c8c7608a5d559f97de
Darnes1958 <abdel_ati_m@yahoo.com>
2025-06-24 21:43:35 +0200

    any

--- app/Http/Livewire/Admin/ToHafitha.php
+++ app/Http/Livewire/Admin/ToHafitha.php
@@ -36,7 +36,8 @@ class ToHafitha extends Component
           if (Auth::user()->company=='Daibany' && $this->TajNo==50) $bankcode=Str::substr($acc, 0, 4);
           else $bankcode=Str::substr($acc, 0, 3);
 
-        if (Auth::user()->company=='BokreahAli' || Auth::user()->company=='Botnan' || Auth::user()->company=='Anwar')
+        if (Auth::user()->company=='BokreahAli' || Auth::user()->company=='Botnan'
+            || Auth::user()->company=='Anwar' || Auth::user()->company=='Rabeeh')
         {
             if (Auth::user()->company=='BokreahAli') {
                 if ($this->TajNo==3)
@@ -64,6 +65,11 @@ class ToHafitha extends Component
                 if ($this->TajNo==2)
                     $bank_no='3';
 
+
+            }
+            if (Auth::user()->company=='Rabeeh') {
+                if ($this->TajNo==1)
+                    $bank_no='3';
 
             }
         }
